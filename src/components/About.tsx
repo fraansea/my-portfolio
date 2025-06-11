99e8d7d91cc0299e8b3a20aa772c858dad6574ad
mohamedshamil7 <mohamedshamil0507@gmail.com>
2025-06-12 02:19:48 +0530

    Update about image alignment

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const About: React.FC = () => {
   return (
-    <section className="flex flex-col   max-w-[1440px] mx-auto">
+    <section className="flex flex-col  max-w-[1440px] mx-auto">
       <h2 className="text-black text-3xl font-bold font-manrope leading-tight tracking-tight mt-20 mb-8">
         About
       </h2>
@@ -12,10 +12,13 @@ export const About: React.FC = () => {
       <p className="text-zinc-600 text-xl font-normal font-manrope leading-8 tracking-wide max-w-2xl mx-auto mb-12">
         I thrive on transforming ideas into reality, whether it's crafting digital interfaces, designing immersive visuals, or building websites that feel effortless to use.
       </p>
+
+      <div className='flex items-center justify-center'>
+
       
-     <div className="relative flex justify-center items-center mt-10 h-[260px] w-[420px] max-w-full">
+     <div className="relative flex justify-center items-center mt-10 h-[320px] w-full max-w-full">
       {/* Left Polaroid */}
-      <div className="absolute left-10 top-6 rotate-[-6deg] z-10 bg-white shadow-md p-2 w-[180px]">
+      <div className="absolute left-36 top-6 rotate-[-6deg] z-10 bg-white shadow-md p-2 w-[180px]">
        <img
         src="/pic1.png"
         alt="Some of my recent photography"
@@ -25,14 +28,15 @@ export const About: React.FC = () => {
       </div>
 
       {/* Right Polaroid */}
-      <div className="absolute left-32 top-[-8px] rotate-[8deg] z-20 bg-white shadow-lg p-2 w-[180px]">
+      <div className="absolute left-80 top-[12px] rotate-[8deg] z-20 bg-white shadow-lg p-2 w-[180px]">
        <img
         src="/pic2.png"
         alt="Some of my recent photography"
         className="w-full h-[180px] object-cover rounded"
-       />
+        />
        <p className="mt-1 text-center text-xs italic">Some of my recent photography</p>
       </div>
+        </div>
       </div>
 
 
